317f66e41615448e8d3484c006df60108d2d303a
MESHEmugles <mdpeter28@gmail.com>
2023-09-02 09:16:10 +0100

    changed things

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,8 @@ import Navbar from '@/components/navbar/Navbar'
 import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
+// const script = Kaushan_Script({ subsets: ['latin'], weight:['400'], display:'swap' })
+// const quick = Quicksand({ subsets: ['latin'], weight:'500', display:'swap' })
 
 export const metadata = {
   title: 'MFM Yaounde',
@@ -14,7 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-
         <div className='container'>
           <Navbar/>
           {children}
